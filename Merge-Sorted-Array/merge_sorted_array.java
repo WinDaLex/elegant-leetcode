787b905cec22a6fd0c2ab6e93bf64fab68d38ae2
WinDaLex <windalex6@gmail.com>
2014-10-20 20:29:46 +0800

    Add Merge-Sorted-Array solution in java

---
+++ Merge-Sorted-Array/merge_sorted_array.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public void merge(int A[], int m, int B[], int n) {
+        while (m > 0 && n > 0) A[m + n - 1] = A[m - 1] > B[n - 1] ? A[--m] : B[--n];
+        while (m > 0) A[m + n - 1] = A[--m];
+        while (n > 0) A[m + n - 1] = B[--n];
+    }
+}

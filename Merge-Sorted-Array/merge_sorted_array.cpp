6831b5d5159e74998cda7f7f15b65f0f8e2443fd
WinDaLex <windalex6@gmail.com>
2014-10-20 20:40:57 +0800

    Update Merge_Sorted_Array solution in cpp

--- Merge-Sorted-Array/merge_sorted_array.cpp
+++ Merge-Sorted-Array/merge_sorted_array.cpp
@@ -1,8 +1,8 @@
 class Solution {
     public:
         void merge(int A[], int m, int B[], int n) {
-            while (m > 0 && n > 0) A[m + n - 1] = A[m - 1] > B[n - 1] ? A[--m] : B[--n];
-            while (m > 0) A[m + n - 1] = A[--m];
-            while (n > 0) A[m + n - 1] = B[--n];
+            while (m && n) A[m + n - 1] = A[m - 1] > B[n - 1] ? A[--m] : B[--n];
+            while (m) A[m + n - 1] = A[--m];
+            while (n) A[m + n - 1] = B[--n];
         }
 };

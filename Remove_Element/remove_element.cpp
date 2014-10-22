55c8745a4645c0d28679ae45e83c7b469ce36ea3
WinDaLex <windalex6@gmail.com>
2014-10-22 23:08:07 +0800

    Modify Remove_Element solution in cpp

--- Remove_Element/remove_element.cpp
+++ Remove_Element/remove_element.cpp
@@ -3,7 +3,7 @@ class Solution {
         int removeElement(int A[], int n, int elem) {
             for (int i = 0; i < n; i++) {
                 while (i < n && A[i] == elem) {
-                    swap(A[i], A[--n]);
+                    A[i] = A[--n];
                 }
             }
             return n;
